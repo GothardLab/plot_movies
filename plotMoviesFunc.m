c4910f27e84dee7c0f7bdf9e22f22cb089fa8653
putnampt <putnampt@gmail.com>
2015-03-09 19:06:24 -0700

    GUI GCBO data linkage complete, started plot fun

---
+++ plotMoviesFunc.m
@@ -0,0 +1,13 @@
+%function [ output_args ] = plotMoviesFunc( input_args )
+%PLOTMOVIESFUNC Summary of this function goes here
+%   Detailed explanation goes here
+
+%Need to write vargin for plotDat or commands
+
+%end
+
+clear all
+clc
+
+load('plotDat.mat');
+
